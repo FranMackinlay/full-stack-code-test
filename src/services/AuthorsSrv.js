28198f36171d5d92ffaf505b883e46e324608cf0
Francisco Mackinlay <franmackinlay@gmail.com>
2021-06-04 21:12:52 +0200

    IMPROVE, authors srv

--- src/services/AuthorsSrv.js
+++ src/services/AuthorsSrv.js
@@ -9,6 +9,41 @@ const AuthorsSrv = {
       url: `${API_URL}/authors`,
     });
     return res;
+  },
+  getAuthor: async id => {
+    const res = await axios({
+      method: 'get',
+      url: `${API_URL}/authors/${id}`,
+    });
+    return res;
+  },
+  upsertAuthor: async author => {
+    if (!author._id) return await AuthorsSrv.createAuthor(author);
+    const res = await axios({
+      method: 'PUT',
+      url: `${API_URL}/authors/${author._id}`,
+      data: {
+        author,
+      },
+    });
+    return res;
+  },
+  createAuthor: async author => {
+    const res = await axios({
+      method: 'POST',
+      url: `${API_URL}/authors`,
+      data: {
+        author,
+      },
+    });
+    return res;
+  },
+  deleteAuthor: async id => {
+    const res = await axios({
+      method: 'DELETE',
+      url: `${API_URL}/authors/${id}`,
+    });
+    return res;
   }
 }
 
