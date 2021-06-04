9dcc4d5e0aa584c1f595549530e9abefd5f5b148
Francisco Mackinlay <franmackinlay@gmail.com>
2021-06-04 17:45:32 +0200

    IMPROVE, books component get books

--- src/components/BooksComponent.js
+++ src/components/BooksComponent.js
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import BooksSrv from '../services/BooksSrv';
 
 export default function BooksComponent() {
+
+
+  const [books, setBooks] = useState([]);
+
+
+  useEffect(() => {
+
+    const getBooks = async () => {
+      const { data: { books } } = await BooksSrv.getBooks();
+      setBooks(books || []);
+    }
+
+    getBooks();
+  }, [])
+
   return (
     <div>
 
