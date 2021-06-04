b06bae393cb7b54893517f438cd99b297042463b
Francisco Mackinlay <franmackinlay@gmail.com>
2021-06-04 21:05:50 +0200

    ADD, authors table

--- src/components/BooksComponent/BooksComponent.jsx
+++ src/components/BooksComponent/BooksComponent.jsx
@@ -3,11 +3,13 @@ import BooksSrv from '../../services/BooksSrv';
 import LoadingBox from '../LoadingBox/LoadingBox';
 import { Link } from 'react-router-dom';
 import './BooksComponent.css';
+import AuthorsSrv from '../../services/AuthorsSrv';
 
 export default function BooksComponent() {
 
 
   const [books, setBooks] = useState([]);
+  const [authors, setAuthors] = useState([]);
 
   useEffect(() => {
 
@@ -16,7 +18,13 @@ export default function BooksComponent() {
       setBooks(books || []);
     }
 
+    const getAuthors = async () => {
+      const {data: { authors } } = await AuthorsSrv.getAuthors();
+      setAuthors(authors || []);
+    }
+
     getBooks();
+    getAuthors();
   }, [])
 
   return (
@@ -31,28 +39,52 @@ export default function BooksComponent() {
           Welcome to the <br /> Soamee Public Library!
         </h1>
       </div>
-      <div className="books-list-container col-md-4 offset-4 text-center">
+      <div className="books-list-container col-md-8 offset-2 text-center">
         {!books.length ? (
           <LoadingBox></LoadingBox>
         ) : (
-          <table className="table table-striped fm-m-0">
-            <thead>
-              <tr>
-                <th scope="col">Books available</th>
-              </tr>
-            </thead>
-            <tbody>
-              {books.map(book => (
-                <tr key={book._id}>
-                  <th>
-                    <Link className="link-clean" to={`/book/${book._id}`} >
-                      {book.name}
-                    </Link>
-                  </th>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          <div className="main-container fm-df fm-alist fm-juce">
+            <div className="table-container">
+              <table className="table table-striped fm-m-0">
+                <thead>
+                  <tr>
+                    <th scope="col">Books available</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {books.map(book => (
+                    <tr key={book._id}>
+                      <th>
+                        <Link className="link-clean" to={`/book/${book._id}`} >
+                          {book.name}
+                        </Link>
+                      </th>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+            <div className="table-container">
+              <table className="table table-striped fm-m-0">
+                <thead>
+                  <tr>
+                    <th scope="col">Authors</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {authors.map(author => (
+                    <tr key={author._id}>
+                      <th>
+                        <Link className="link-clean" to={`/author/${author._id}`} >
+                          {author.first_name} {author.last_name}
+                        </Link>
+                      </th>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
         )}
       </div>
     </div>
