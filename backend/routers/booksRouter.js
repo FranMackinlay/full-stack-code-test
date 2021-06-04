a8aeef4fba11921d1312dcbf1d3842f27a50f5e5
Francisco Mackinlay <franmackinlay@gmail.com>
2021-06-04 17:30:47 +0200

    ADD, books router to backend

--- backend/routers/booksRouter.js
+++ backend/routers/booksRouter.js
@@ -1,28 +1,33 @@
-var express = require('express');
-var router = express.Router();
+import express from 'express';
+import expressAsyncHandler from 'express-async-handler';
+import Book from '../models/BooksSchema.js';
+import Author from '../models/AuthorSchema.js';
+
+
+const router = express.Router();
 
 /* GET Books */
-router.get('/', function (req, res, next) {
-
-  res.render('index', { title: 'Express' });
-});
+router.get('/', expressAsyncHandler(async (req, res) => {
+  const books = await Book.find()
+  res.send({ books });
+}));
 
 /* GET book by _id */
-router.get('/:bookId', function (req, res, next) {
+router.get('/:bookId', function (req, res) {
 
-  res.render('index', { title: 'Express' });
+  res.send('index', { title: 'Express' });
 });
 
 /* Post new book */
-router.post('/', function (req, res, next) {
+router.post('/', function (req, res) {
 
-  res.render('index', { title: 'Express' });
+  res.send('index', { title: 'Express' });
 });
 
 /* PUT update book by _id */
-router.put('/:bookId', function (req, res, next) {
+router.put('/:bookId', function (req, res) {
 
-  res.render('index', { title: 'Express' });
+  res.send('index', { title: 'Express' });
 });
 
 /* DELETE book by _id. */
@@ -33,4 +38,4 @@ router.delete('/:bookId', function (req, res, next) {
 
 
 
-module.exports = router;
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import booksRouter from './routers/booksRouter.js';
 
 dotenv.config();
 
@@ -9,6 +10,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/books', booksRouter);
 
 
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/soamee', {
