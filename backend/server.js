9e0736221f86d075cf1bbe245c0b79e41c1a0c38
Francisco Mackinlay <franmackinlay@gmail.com>
2021-06-04 16:46:23 +0200

    ADD, backend api

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/', (req, res) => {
+  res.send('Server is ready!');
+});
+
+app.use((error, req, res, next) => {
+  res.status(500).send({ message: error.message });
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
+});
